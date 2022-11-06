d84edc9b2b02ef856122075137b502b2af50ac39
Cole Killian <g.cole.killian@gmail.com>
2022-11-05 21:23:44 -0700

    color theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,21 @@
 // theme.js
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 // Version 1: Using objects
 export const theme = extendTheme({
   colors: {
-    backgroundDark: '#1F1B24',
+    backgroundDark: "#1F1B24",
   },
   styles: {
     global: {
       body: {
-        bg: '#1F1B24',
-        color: 'white'
-      }
-    }
-  }
-})
+        bg: "#1F1B24",
+        color: "white",
+      },
+    },
+  },
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
