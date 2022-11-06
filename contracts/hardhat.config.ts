2b886acc88adde3fbbd2d4101e637e4303defaa9
taijusanagi <taijusanagi@gmail.com>
2022-11-06 08:06:43 -0800

    fix the mnemonic getting in hardhat for deploy

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -6,7 +6,11 @@ import { HardhatUserConfig } from "hardhat/config";
 
 import rpc from "./config/rpc.json";
 
-const mnemonic = fs.readFileSync("../mnemonic.txt", "ascii").trim();
+const mnemonicFileName = "../mnemonic.txt";
+let mnemonic = "test ".repeat(11) + "junk";
+if (fs.existsSync(mnemonicFileName)) {
+  mnemonic = fs.readFileSync("../../mnemonic.txt", "ascii").trim();
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
